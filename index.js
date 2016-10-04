fa857f34d9bf5c08ce98dc2399fb19fd71834a44
Carlo Cruz <me@carlocruz.com>
2016-10-04 17:17:01 +1100

    Add google directions

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 var restify = require('restify');
-var http = require('http');
-
-function respond(req, res, next) {
-  res.send('hello ' + req.params.name);
-  next();
-}
 
 function getForecast(req, res) {
-  client = restify.createClient({
+  var client = restify.createClient({
     url: 'http://api.wunderground.com',
   });
 
   client.get('/api/6374df36c9e7e429/forecast/q/zmw:00000.1.95864.json', function (err, request) {
     request.on('result', function (err, response) {
+      console.log(err);
       response.body = '';
       response.on('data', function (chunk) {
         response.body += chunk;
@@ -25,9 +20,30 @@ function getForecast(req, res) {
   });
 }
 
+function getDirectionsForDulux(req, res) {
+  var client = restify.createClient({
+    url: 'https://maps.googleapis.com',
+  });
+
+  client.get('', function (err, request) {
+   request.on('result', function(err, response) {
+     console.log(err);
+     response.body = '';
+     response.on('data', function(chunk) {
+       response.body += chunk;
+     });
+
+     response.on('end', function() {
+       res.send(JSON.parse(response.body));
+     });
+   })
+  })
+}
+
 var server = restify.createServer();
 server.get('/weather/10day', getForecast);
+server.get('/directions/dulux', getDirectionsForDulux);
 
-server.listen(8080, function() {
+server.listen(8080, function () {
   console.log('%s listening at %s', server.name, server.url);
 });
