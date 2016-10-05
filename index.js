07fbba09a53f5f61ccd26f21963c47d9a8fe3676
Carlo Cruz <me@carlocruz.com>
2016-10-05 15:17:56 +1100

    Add 10 day forcast

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 var restify = require('restify');
 var config = require('./config/my.config.js');
 
-function getForecast(req, res) {
+function getForecast() {
+  var client = restify.createClient({
+    url: url
+  });
+
+  var weatherUrl = '/api/'+config.wunderground.api+'/forecast/q/'+config.wunderground.closestWeatherStation+'.json';
+  client.get(weatherUrl, function (err, request) {
+    request.on('result', function (err, response) {
+      response.body = '';
+      response.on('data', function (chunk) {
+        response.body += chunk;
+      });
+
+      response.on('end', function () {
+        res.send(JSON.parse(response.body));
+      });
+    });
+  });
+}
+
+function getForecast10day(req, res) {
   var client = restify.createClient({
     url: 'http://api.wunderground.com',
   });
 
-  var weatherUrl = '/api/'+config.wunderground.api+'/forecast/q/'+config.wunderground.closestWeatherStation+'.json';
+  var weatherUrl = '/api/'+config.wunderground.api+'/forecast10day/q/'+config.wunderground.closestWeatherStation+'.json';
   client.get(weatherUrl, function (err, request) {
     request.on('result', function (err, response) {
       response.body = '';
@@ -42,7 +62,8 @@ function getDirectionsForDulux(req, res) {
 }
 
 var server = restify.createServer();
-server.get('/weather/10day', getForecast);
+server.get('/weather/10day', getForecast10day);
+server.get('/weather/forecast', getForecast);
 server.get('/directions/dulux', getDirectionsForDulux);
 
 server.listen(8080, function () {
